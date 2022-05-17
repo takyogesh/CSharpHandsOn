dc6bebb9b49bfe9d26fcb4d1605eade9f01b4c8c
Yogesh <takyogesh@github.com>
2022-05-17 15:50:10 +0530

    Basic OOOPs hands-On

---
+++ OopsBasicConsole/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsBasicConsole
+{
+    public class Calculator
+    {
+       public String add;
+       public  String sub;
+       public String div;
+       public String multi;
+       
+       public void Addition(int num1,int num2)
+        {
+            Console.WriteLine(add+": "+(num1 + num2));
+        }
+        public void Subtraction(int num1,int num2)
+        {
+            Console.WriteLine(sub+": "+(num1 - num2));
+        }
+        public void Division(int num1,int num2)
+        {
+            Console.WriteLine(div+": "+(num1 / num2));
+        }
+        public void Multiplication(int num1,int num2)
+        {
+            Console.WriteLine(multi+": "+(num1 * num2));
+        }
+
+    }
+}
---
+++ OopsBasicConsole/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsBasicConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program pro = new Program();
+            pro.calculate();
+
+            int n1 = 100;
+            int n2 = 50;
+
+            Calculator calc = new Calculator();
+            calc.add = "Addition";
+            calc.Addition(n1, n2);
+            calc.sub = "Subtraction";
+            calc.Subtraction(n1, n2);
+            calc.multi = "Multiplication";
+            calc.Multiplication(n1, n2);
+            calc.div = "Division";
+            calc.Division(n1, n2);
+      
+            Console.Read();
+        }
+
+       void calculate()
+        {
+            Console.WriteLine("Calculator Function");
+        }
+    }
+}
