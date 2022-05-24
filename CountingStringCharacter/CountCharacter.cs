4107f870d2750bd6b687c03ffe1c2a2188dff392
Yogesh <takyogesh@github.com>
2022-05-24 17:52:53 +0530

    counting the character of string by passing the input,by read the file and by taking input from the user

---
+++ CountingStringCharacter/CountCharacter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.IO;
+
+namespace CountingStringCharacter
+{
+    class CountCharacter
+    {
+
+        public String read()
+        {
+            FileStream fs = new FileStream(@"C:\CustomFiles\CharCount.txt", FileMode.Open,FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            String s = sr.ReadLine();
+            fs.Close();
+            sr.Close();
+            return s;
+        }
+        public void count(String s)
+        {
+            char[] ch = s.ToCharArray();
+
+         
+            List<char> charList = new List<char>();
+            List<char> charList1 = new List<char>();
+
+            foreach (char c in ch)
+                charList.Add(c);
+           
+            for (int i=0; i<charList.Count; i++)
+            {
+                   if(!charList1.Contains(charList[i]))
+                        charList1.Add(charList[i]);
+            }
+
+            Dictionary<char, int> d = new Dictionary<char, int>();
+
+            int count = 0;
+            for(int i=0; i<charList1.Count;i++)
+            {
+                for(int j=0;j<charList.Count;j++)
+                {
+                    if (charList1[i] == charList[j])
+                        count++;
+                }
+                d.Add(charList1[i],count);
+                count = 0;
+
+            }
+            foreach(var di in d)
+            {
+                Console.WriteLine(di.Key+": "+di.Value);
+            }
+          
+        }
+    }
+}
---
+++ CountingStringCharacter/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountingStringCharacter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CountCharacter cs = new CountCharacter();
+            // count the char of string from console i.e take input from user
+            Console.WriteLine("----------------------------Enter the String");
+            cs.count(Console.ReadLine());
+
+            Console.WriteLine("----------------------------by reading txt file");
+            //by reading string from txt file 
+             string s=  cs.read();
+            cs.count(s);
+            Console.WriteLine("----------------------------by passing string");
+            //by passing the input 
+            cs.count("Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+                "Nunc accumsan sem ut ligula scelerisque sollicitudin." +
+                " Ut at sagittis augue. Praesent quis rhoncus justo. " +
+                "Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. " +
+                "Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. " +
+                "Donec nec velit non ligula efficitur luctus.");
+
+
+            Console.Read();
+        }
+    }
+}
