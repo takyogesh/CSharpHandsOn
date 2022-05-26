a357f09406a672fa089d96088f92ff6b500ff74e
Yogesh <takyogesh@github.com>
2022-05-26 17:19:57 +0530

    EbBill Project with the DDL using class library

---
+++ DDLhandsON/DDLhandsOn/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EBBill;
+namespace DdlHandsOn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ebill e = new Ebill();
+            e.CustomerData(101, "amar", 10);
+            e.CustomerData(102, "rahul", 9);
+            e.CustomerData(103, "akshay", 27);
+            e.CustomerData(104, "vinay", 12);
+            e.CustomerData(105, "ankit", 32);
+            e.CustomerData(106, "yogi", 18);
+            e.CustomerData(107, "ramesh", 21);
+            e.CustomerData(108, "suresh", 19);
+            e.CustomerData(109, "akash", 13);
+            TOP:
+            Console.WriteLine("choose the option: \n 1 for Customer detalis \n 2 for show EBbill \n 3 for all bill report\n");
+            int i = Convert.ToInt32(Console.ReadLine());
+            switch (i)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("Enter customer id to view details");
+                        int id = Convert.ToInt32(Console.ReadLine());
+                        e.ShowCustomerDetails(id);
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("Enter customer id to view details");
+                        int id = Convert.ToInt32(Console.ReadLine());
+                        e.ShowCustomerDetails(id);
+                        break;
+                    }
+                case 3:
+                    {
+                        e.ShowEbBillReport();
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("choose correct option");
+                        goto TOP;
+                    }
+            }
+            Console.Read();
+
+        }
+    }
+}
---
+++ DDLhandsON/EBBill/Ebill.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace EBBill
+{
+    public class Ebill
+    {
+       public int customerID { get; set; }
+        public string customerName { get; set; }
+        public int noOfUnits { get; set; }
+        public int unitsPerCost { get; set; } = 10;
+        public int total { get; set; }
+
+        List<Ebill> ebill = new List<Ebill>();
+
+        private Ebill(int customerID, string customerName, int noOfUnits)
+        {
+            this.customerID = customerID;
+            this.customerName = customerName;
+            this.noOfUnits = noOfUnits;
+            tBill();
+        }public Ebill() { }
+
+        public void CustomerData(int iD,String name,int units)
+        {
+            Ebill bill = new Ebill(iD, name, units);
+            ebill.Add(bill);
+        }
+        private void tBill()
+        {
+            total = unitsPerCost * noOfUnits;
+        }
+        public void ShowCustomerDetails(int customerID)
+        {
+            foreach(var data in ebill)
+            {
+                if (data.customerID == customerID)
+                    Console.WriteLine("CustomerID :" + data.customerID + "\tCustomerName :" + data.customerName + "\tUsage in Units :" + data.noOfUnits);
+            }
+        }
+        public void ShowEbBill(int customerID)
+        {
+            foreach (var data in ebill)
+            {
+                if (data.customerID == customerID)
+                    Console.WriteLine("CustomerID :" + data.customerID + "\tCustomerName :" + data.customerName + "\tUsage in Units :" + data.noOfUnits+ "\tTotalBill :"+ data.total);
+            }
+        }
+        public void ShowEbBillReport()
+        {
+            foreach (var data in ebill)
+            {
+                    Console.WriteLine("CustomerID :" + data.customerID + "\tCustomerName :" + data.customerName + "\tUsage in Units :" + data.noOfUnits + "\tTotalBill :" + data.total);
+            }
+
+        }
+
+    }
+}
