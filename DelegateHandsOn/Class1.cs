5dced6d254b92e87cc406deb21bc6db1143f2849
Yogesh <takyogesh@github.com>
2022-06-13 10:55:28 +0530

    Delegates HandsOn with both return n void type

---
+++ DelegateHandsOn/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateHandsOn
+{
+    delegate void message(string msg);
+    class Class1
+    {
+        public void Hello(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+        public void Hii(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+        public void Gm(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+
+    }
+}
---
+++ DelegateHandsOn/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateHandsOn
+{
+    class Program
+    {
+        delegate int number(int n1,int n2);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("delegates with return type");
+            number n = new number(multi);
+            Console.WriteLine(n(10, 35));
+            number n1 = new number(add);
+            Console.WriteLine(n1(10, 35));
+
+            Console.WriteLine("delegates with non-return type");
+
+            Class1 c1 = new Class1();
+            message m1 = new message(c1.Hello);
+            m1("hello");
+            message m2 = new message(c1.Gm);
+            m2("Good morning");
+            Console.Read();
+        }
+
+        public static int  add(int a, int b)
+        {
+            return a + b;
+        } 
+        public static int  multi(int a, int b)
+        {
+            return a * b;
+        } public static int  divi(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
