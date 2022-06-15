dd241410b67941d1e684789a5e5c9d5c6f0fde88
Yogesh <takyogesh@github.com>
2022-06-15 12:16:05 +0530

    Hands on Entity framework add the object to the database, I had use the different approach add object using paramaterize constructor and print the object using override ToString method

---
+++ EntityFrameworkProject/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bankDBEntities bankDB = new bankDBEntities();
+
+            //to add info into account table
+            //bankDB.accounts.Add(new account(104,"5556667772","sihor","Indore"));
+            //bankDB.SaveChanges();
+
+            //to view all table data
+            var acctList=bankDB.accounts.ToList();
+            foreach(var l in acctList)
+            {
+                Console.WriteLine(l);
+            }
+            Console.WriteLine();
+
+            //bankDB.employees.Add(new employee(105, "parag", "parag@123", "6668889991", "Yawal"));
+            //bankDB.SaveChanges();
+            var empList = bankDB.employees.ToList();
+            foreach (var l in empList)
+            {
+                Console.WriteLine(l);
+            }
+            Console.Read();
+        }
+    }
+}
